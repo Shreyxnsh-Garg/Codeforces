b31d3d985eb4811dff75e32325a29a6c98c4ec3c
Shreyxnsh-Garg <shreyansh7952@gmail.com>
2025-10-14 19:38:53 +0530

    Create 1030A.c

---
+++ 1030A.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, a, count = 0;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a);
+        if (a == 1)
+        {
+            count++;
+        }
+    }
+    if (count >= 1)
+    {
+        printf("HARD");
+    }
+    else
+    {
+        printf("EASY");
+    }
+    return 0;
+}
