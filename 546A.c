286656e5aea5d5ca9238d341998722f9116bea8c
Shreyxnsh-Garg <shreyansh7952@gmail.com>
2025-10-15 19:06:25 +0530

    Create 546A.c

---
+++ 546A.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n,k,w,pay=0;
+    scanf("%d %d %d",&k,&n,&w);
+    for(int i=1;i<=w;i++){
+        pay += k*i;
+    }
+
+    if(pay <=n ){printf("0");}
+    else {printf("%d",pay-n);}
+    return 0;
+}
