62eed5c386a55799e133c91e8b3b26c5a121d4bd
Shreyxnsh-Garg <shreyansh7952@gmail.com>
2025-10-16 00:04:56 +0530

    Create 1950A.c

---
+++ 1950A.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, a, b, c;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+
+        scanf("%d%d%d", &a, &b, &c);
+        if (a < b && b < c)
+        {
+            printf("STAIR\n");
+        }
+        else if (a < b && b > c)
+        {
+            printf("PEAK\n");
+        }
+        else
+        {
+            printf("NONE\n");
+        }
+    }
+    return 0;
+}
