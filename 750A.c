0e121f7a9e22e59965d0ababff15f3fb00f45763
Shreyxnsh-Garg <shreyansh7952@gmail.com>
2025-10-24 12:09:12 +0530

    Create 750A.c

---
+++ 750A.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n,k;
+    scanf("%d %d",&n,&k);
+    int time=240-k;
+    int count = 0;
+    for(int i=1;i<=n;i++){
+        time = time-5*i;
+        if(time>=0)count++;
+
+    }
+    printf("%d",count);
+    return 0;
+}
