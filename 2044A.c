fa4b3b59640b926361771854687db77af7f90220
Shreyxnsh-Garg <shreyansh7952@gmail.com>
2025-10-25 10:22:16 +0530

    Create 2044A.c

---
+++ 2044A.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, a, b, k;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        int count = 0;
+        scanf("%d", &k);
+        for (a = 0; a < k; a++)
+        {
+            for (b = 0; b < k; b++)
+            {
+                if (a + b == k)
+                {
+                    count += 1;
+                }
+            }
+        }
+        printf("%d\n", count);
+    }
+    return 0;
+}
