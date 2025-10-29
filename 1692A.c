f66a861eea53c32fe9c42d5f0a18731766750479
Shreyxnsh-Garg <shreyansh7952@gmail.com>
2025-10-29 21:18:04 +0530

    Create 1692A.c

---
+++ 1692A.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,c,d;
+    int t;
+    scanf("%d",&t);
+    for(int i=1;i<=t;i++){
+        int count=0;
+        scanf("%d%d%d%d",&a,&b,&c,&d);
+        if(a<b){count++;}
+        if(a<c){count++;}
+        if(a<d){count++;}
+        printf("%d\n",count);
+    }
+    return 0;
+}
