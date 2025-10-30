b29ee5a4d885045be875d43efee66fd3e3d091e9
Shreyxnsh-Garg <shreyansh7952@gmail.com>
2025-10-30 20:56:46 +0530

    Create 1807A.c

---
+++ 1807A.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int n,a,b,c;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        scanf("%d %d %d",&a,&b,&c);
+        if(a+b==c){printf("+\n");}
+        if(a-b==c){printf("-\n");}
+    }
+    return 0;
+}
