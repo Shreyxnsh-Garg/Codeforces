8cf168c3a1790334dacb3a086caab3f3c58b510f
Shreyxnsh-Garg <shreyansh7952@gmail.com>
2025-11-06 22:12:27 +0530

    Create 116A.c

---
+++ 116A.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n,a,b,in=0,cap=0;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        scanf("%d%d",&a,&b);
+        if(a<=in)
+        in = in-a;
+        in = in+b;
+        if(in>cap){cap = in;}
+    }
+    printf("%d",cap);
+    return 0;
+}
