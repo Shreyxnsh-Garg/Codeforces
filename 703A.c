f3c6c0377724fd27ab383cd5f77f0001221aa154
Shreyxnsh-Garg <shreyansh7952@gmail.com>
2025-11-10 17:14:11 +0530

    Create 703A.c

---
+++ 703A.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n,m,c,count1=0,count2=0;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d %d",&m,&c);
+        if(m>c){count1++;}
+        if(c>m){count2++;}
+    }
+    if(count1>count2){printf("Mishka");}
+    if(count2>count1){printf("Chris");}
+    if(count1==count2){printf("Friendship is magic!^^");}
+    return 0;
+}
