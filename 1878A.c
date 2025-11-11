cf9cde685f2c0aad9d292fc5e6e8aad1f180e257
Shreyxnsh-Garg <shreyansh7952@gmail.com>
2025-11-11 22:13:52 +0530

    Create 1878A.c

---
+++ 1878A.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int t, n, k;
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++)
+    {
+        scanf("%d %d", &n, &k);
+        int arr[n];
+        int count = 0;
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &arr[j]);
+            if (k == arr[j])
+            {
+                count++;
+            }
+        }
+        if (count > 0)
+        {
+            printf("YES\n");
+        }
+        else
+        {
+            printf("NO\n");
+        }
+    }
+    return 0;
+}
