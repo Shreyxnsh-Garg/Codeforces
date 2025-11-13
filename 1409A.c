23a6730b5de4e964dd0f7e91e8e9b12501537970
Shreyxnsh-Garg <shreyansh7952@gmail.com>
2025-11-13 19:02:21 +0530

    Create 1409A.c

---
+++ 1409A.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int t;
+    scanf("%d", &t);
+ 
+    while (t--)
+    {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        int diff = a - b;
+        if (diff < 0)
+            diff = -diff;
+        int moves = (diff + 9) / 10;
+ 
+        printf("%d\n", moves);
+    }
+ 
+    return 0;
+}
