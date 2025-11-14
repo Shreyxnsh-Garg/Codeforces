fce9b7af3fe249f4938883216968a6eee1a67fc7
Shreyxnsh-Garg <shreyansh7952@gmail.com>
2025-11-14 23:24:29 +0530

    Create 1624A.c

---
+++ 1624A.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ 
+int main() {
+    int t,n;
+    scanf("%d",&t);
+    for(int i=0;i<t;i++){
+        scanf("%d",&n);
+        long long int arr[n];
+        for(int j=0;j<n;j++){
+            scanf("%lld",&arr[j]);
+        }
+        int max=arr[0];
+        int min=arr[0];
+        for(int k=0;k<n;k++){
+            if(arr[k]>max){max = arr[k];}
+            if(arr[k]<min){min = arr[k];}
+        }
+        printf("%d\n",max-min);
+    }
+    return 0;
+}
