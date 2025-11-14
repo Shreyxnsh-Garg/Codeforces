c044c0ef22e31605eb192e1cb6e0fc71176d2d4d
Shreyxnsh-Garg <shreyansh7952@gmail.com>
2025-11-14 23:25:09 +0530

    Update 1624A.c

--- 1624A.c
+++ 1624A.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
- 
+                                                                              //Problem 1624A - Plus One on the subset
 int main() {
     int t,n;
     scanf("%d",&t);
