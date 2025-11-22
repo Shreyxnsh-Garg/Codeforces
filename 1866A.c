7319762312be061c67e13cbaab5e5ae5790c52ce
Shreyxnsh-Garg <shreyansh7952@gmail.com>
2025-11-22 19:36:05 +0530

    Create 1866A.c

---
+++ 1866A.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,min;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+        
+        if (arr[i] < 0)
+        {
+            arr[i] = -arr[i];
+        }
+         min = arr[0];
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] < min)
+            {
+                min = arr[i];
+                
+            }
+        }
+    }
+    printf("%d", min);
+    return 0;
+}
