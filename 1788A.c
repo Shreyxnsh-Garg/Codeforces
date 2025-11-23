b45be6cce32982c3d57dc5c942fc8c6a9c99cc88
Shreyxnsh-Garg <shreyansh7952@gmail.com>
2025-11-23 19:58:07 +0530

    Create 1788A.c

---
+++ 1788A.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+ 
+int main(){
+    int t,n;
+    scanf("%d",&t);
+    for(int j=1;j<=t;j++){
+        scanf("%d",&n);
+        int arr[n];
+        int count=0,count1=0,k;
+        for(int i=0;i<n;i++){
+            scanf("%d",&arr[i]);
+            if(arr[i]%2==0)
+            count++;
+        }
+        if(count==0)
+        printf("1\n");
+        else if(count%2!=0)
+        printf("-1\n");
+        else{
+             for(int i=0;i<n;i++){
+            if(arr[i]%2==0){
+            count1++;
+            if(count1==count/2)
+            k=i+1;}
+        }
+        printf("%d\n",k);
+        }
+    }
+    return 0;
+}
