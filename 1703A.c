4ee5c1b01bfa9dfd44421ebfec7df68d7d1b7f20
Shreyxnsh-Garg <shreyansh7952@gmail.com>
2025-11-26 20:30:56 +0530

    Create 1703A.c

---
+++ 1703A.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++)
+    {
+        char s[10];
+        scanf(" %s", s);
+
+        if ((s[0] == 'y' || s[0] == 'Y') && (s[1] == 'e' || s[1] == 'E') && (s[2] == 's' || s[2] == 'S'))
+        {
+            printf("YES\n");
+        }
+        else
+        {
+            printf("NO\n");
+        }
+    }
+    return 0;
+}
