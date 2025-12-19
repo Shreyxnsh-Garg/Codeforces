4a72e63994226f327c9b509a9e369b1f27d820d5
Shreyxnsh-Garg <shreyansh7952@gmail.com>
2025-12-19 13:39:54 +0530

    Create 1977A.c

---
+++ 1977A.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    int m, n;
+    for (int i = 1; i <= t; i++)
+    {
+        scanf("%d %d", &n, &m);
+        if (n >= m)
+        {
+            if ((n - m) % 2 == 0)
+                printf("YES\n");
+            else
+                printf("NO\n");
+        }
+        else
+            printf("NO\n");
+    }
+    return 0;
+}
