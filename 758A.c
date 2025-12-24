9ed887f1f57ff938c70b0d71ace90e0b2b75d578
Shreyxnsh-Garg <shreyansh7952@gmail.com>
2025-12-24 15:53:22 +0530

    Update 758A.c

--- 758A.c
+++ 758A.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+                                                                                    //Problem 758A - Holiday of Equality
 int main() {
     int n;
     scanf("%d",&n);
