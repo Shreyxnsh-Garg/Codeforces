86de128130ba6c65d78bf58379abc2a52c51cebd
Shreyxnsh-Garg <shreyansh7952@gmail.com>
2025-12-26 19:05:56 +0530

    Create 1791C.c

---
+++ 1791C.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    for(int k=0;k<t;k++){
+        int n;
+        
+        scanf("%d", &n);
+        char s[n];
+        scanf("%s", s);
+
+        int i = 0, j = n - 1;
+
+        while (i < j && s[i] != s[j]) {
+            i++;
+            j--;
+        }
+
+        printf("%d\n", j - i + 1);
+    }
+
+    return 0;
+}
