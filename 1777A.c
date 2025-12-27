89f12a6f7318f45477c3644d4a6030d3c221690f
Shreyxnsh-Garg <shreyansh7952@gmail.com>
2025-12-27 20:05:41 +0530

    Create 1777A.c

---
+++ 1777A.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d",&t);
+    for(int k=0;k<t;k++){
+        int n;
+        scanf("%d",&n);
+        int arr[n];
+        for(int j=0;j<n;j++){
+            scanf("%d",&arr[j]);
+        }
+
+        int count=0;
+        for(int i=0;i<n-1;i++){
+            if(arr[i]%2==0 && arr[i+1]%2==0){count++;}
+            if(arr[i]%2!=0 && arr[i+1]%2!=0){count++;}
+            
+        }
+        printf("%d\n",count);
+    }
+    return 0;
+}
