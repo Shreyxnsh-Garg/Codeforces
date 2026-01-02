2d94276b9a675c407f047bc96e445ec52b7f1acd
Shreyansh Garg <shreyansh7952@gmail.com>
2026-01-02 16:02:51 +0530

    Create 1857A.cpp

---
+++ 1857A.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+
+    for(int i=0;i<t;i++){
+        int n;
+        cin>>n;
+
+        int arr[n];
+        for(int j=0;j<n;j++){
+            cin>>arr[j];
+        }
+
+        int odd_count=0;
+        for(int j=0;j<n;j++){
+            if(arr[j]%2 !=0){
+                odd_count++;
+            }
+        }
+
+        if(odd_count%2==0){
+            cout<<"YES"<<endl;   
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
