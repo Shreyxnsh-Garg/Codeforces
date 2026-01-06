d0b74b1142eea98f4cbdf4e071baf7ef16c613ff
Shreyansh Garg <shreyansh7952@gmail.com>
2026-01-06 10:21:46 +0530

    Create 1829B.cpp

---
+++ 1829B.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+
+    for(int i=0;i<t;i++){
+        int n;
+        cin>>n;
+
+        int arr[n];
+        int count = 0,maxCount=0;
+        for(int j=0;j<n;j++){
+            cin>>arr[j];
+            if(arr[j] == 0){
+                count++;
+                if(count > maxCount){
+                    maxCount = count;
+                }
+            }
+            if(arr[j] != 0){
+                count = 0;
+            }
+        }
+        cout<<maxCount<<endl;
+    }
+    return 0;
+}
