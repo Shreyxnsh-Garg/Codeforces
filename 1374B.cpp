9ae47ba44518eb5f7870e6271ab8db6b29b56058
Shreyansh Garg <shreyansh7952@gmail.com>
2026-01-09 00:05:48 +0530

    Create 1374B.cpp

---
+++ 1374B.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        long long n;
+        cin >> n;
+        int moves = 0;
+        while (n > 1)
+        {
+            if (n % 6 == 0)
+                n /= 6;
+            else if (n % 3 == 0)
+                n *= 2;
+            else
+            {
+                moves = -1;
+                break;
+            }
+            moves++;
+        }
+        cout << moves << '\n';
+    }
+}
