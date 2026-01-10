c994a362b3efcc5d9d3ba2d8eb8197b12e2415f4
Shreyansh Garg <shreyansh7952@gmail.com>
2026-01-11 02:22:03 +0530

    Implement solution for problem 1840A

---
+++ 1840A.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        string s;
+        cin >> n >> s;
+        
+        string ans = "";
+        int i = 0;
+        
+        while(i < n) {
+            ans += s[i];
+            int j = i + 1;
+            while(j < n && s[j] != s[i]) {
+                j++;
+            }
+            i = j + 1;
+        }
+        
+        cout << ans << endl;
+    }
+    return 0;
+}
